94af8d7ef0ef84e9de66b8ae1b0356f95233d5fd
Sav <maidenfan78@gmail.com>
2025-06-29 11:32:12 +1000

    Minor changes

--- compile.bat
+++ compile.bat
@@ -1,7 +1,15 @@
 @echo off
-echo Compiling EA...
-"E:\GO_MT4\metaeditor.exe" /compile:"E:\CCTS_EA_Framework\MQL4\Experts\CCTS_Breakout.mq4" >> compile_log.txt 2>&1
+rem Allow overriding metaeditor_path from the environment
+if not defined metaeditor_path (
+    set "metaeditor_path=E:\MT4_4.1_STD_1\metaeditor.exe"
+)
 
-echo Done. Opening compile_log.txt...
-notepad compile_log.txt
+rem Use an absolute path to the EA source based on this script's location
+set "mq4_file=%~dp0MQL4\Experts\CCTS_Breakout.mq4"
+set "log_file=%~dp0compile_log.txt"
+
+echo Compiling "%mq4_file%" ...
+"%metaeditor_path%" /compile:"%mq4_file%" > "%log_file%" 2>&1
+
+echo Done. See compile_log.txt for details.
 pause
